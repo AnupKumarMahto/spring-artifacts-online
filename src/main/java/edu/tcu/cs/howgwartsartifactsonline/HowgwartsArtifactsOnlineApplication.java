96c85adf53afb96adbeaa7a20afaa140fad4d0e4
Your Name <you@example.com>
2023-09-26 18:54:08 +0530

    This is the first commit.

---
+++ src/main/java/edu/tcu/cs/howgwartsartifactsonline/HowgwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.howgwartsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowgwartsArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowgwartsArtifactsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tcu/cs/howgwartsartifactsonline/HowgwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.howgwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowgwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
